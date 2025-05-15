1a2e117dab58fc8e413c2148526221f41071baf7
NagmeKaraaslan <karaaslannagme@gmail.com>
2025-05-15 22:57:39 +0300

    a tiny progress still a progress :)

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns= [
     path('main/', views.main_page, name='blog_main'),
     path( 'newpost/', views.new_post, name='newpost'),
+    path('', include('blog_app.urls')),
 ]
